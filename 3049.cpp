5a810ec693ba9910bb86e143778e99a1c5dc00b2
Davi <davi.crdm@gmail.com>
2025-05-04 12:40:55 -0300

    3049

--- 3049.cpp
+++ 3049.cpp
@@ -4,14 +4,19 @@ using namespace std;
 
 int main(){
 
+    int b, t;
 
+    cin >> b;
+    cin >> t;
 
+    if( (b + t) * (70 / 2) > 11200 / 2)
+        cout << 1 << endl;
 
-
-
-
-
-
-
+    else if((b + t) * (70 / 2) < 11200 / 2)
+        cout << 2 << endl;
+    
+    else
+        cout << 0 << endl;
+        
     return 0;
 }
