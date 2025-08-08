7f8e5a4572e6f19926f16b54eb46338b6aad9e1f
Davi Manzini <dcrdm24@inf.ufpr.br>
2025-08-08 17:02:40 -0300

    problema 231a do codeforces

---
+++ 231a.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    int total = 0;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+
+        int count = 0;
+        for(int j = 0; j < 3; j++){
+
+            int binario;
+            cin >> binario;
+            if(binario == 1){
+                count++;
+            }
+        }
+
+        if(count >= 2){
+            total++;
+        }
+    }
+
+    cout << total << endl;
+
+    return 0;
+}
